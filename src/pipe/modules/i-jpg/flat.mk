3233fca519b27db4920c57729bb8b8ad6e6ad2f7
Paolo DePetrillo <paolod@gmail.com>
2023-12-02 11:47:27 -0500

    Static libjpeg for Android

--- src/pipe/modules/i-jpg/flat.mk
+++ src/pipe/modules/i-jpg/flat.mk
@@ -1,3 +1,10 @@
-MOD_CFLAGS=$(shell pkg-config --cflags libjpeg)
-MOD_LDFLAGS=$(shell pkg-config --libs libjpeg)
-pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h
+ifeq ($(TARGET),aarch64-linux-android)
+  MOD_CFLAGS=-I$(LIBJPEG_TURBO_I) -I$(LIBJPEG_TURBO_L)
+  MOD_LDFLAGS=-L$(LIBJPEG_TURBO_L) -ljpeg
+  EXTRA_DEPS=$(LIBJPEG_TURBO_L)/libjpeg.a
+else
+  MOD_CFLAGS=$(shell pkg-config --cflags libjpeg)
+  MOD_LDFLAGS=$(shell pkg-config --libs libjpeg)
+  EXTRA_DEPS=
+endif
+pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h $(EXTRA_DEPS)
--- src/pipe/modules/o-jpg/flat.mk
+++ src/pipe/modules/o-jpg/flat.mk
@@ -1,2 +1,10 @@
-MOD_CFLAGS=$(shell pkg-config --cflags libjpeg)
-MOD_LDFLAGS=$(shell pkg-config --libs libjpeg)
+ifeq ($(TARGET),aarch64-linux-android)
+  MOD_CFLAGS=-I$(LIBJPEG_TURBO_I) -I$(LIBJPEG_TURBO_L)
+  MOD_LDFLAGS=-L$(LIBJPEG_TURBO_L) -ljpeg
+  EXTRA_DEPS=$(LIBJPEG_TURBO_L)/libjpeg.a
+else
+  MOD_CFLAGS=$(shell pkg-config --cflags libjpeg)
+  MOD_LDFLAGS=$(shell pkg-config --libs libjpeg)
+  EXTRA_DEPS=
+endif
+pipe/modules/o-jpg/libo-jpg.so: $(EXTRA_DEPS)
