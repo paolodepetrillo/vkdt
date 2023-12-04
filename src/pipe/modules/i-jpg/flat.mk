aef403bbd7ea473c4d681d3ea8da1c0f0f3ec718
Paolo DePetrillo <paolod@gmail.com>
2023-12-03 20:34:09 -0500

    Fix static libjpeg

--- src/pipe/modules/i-jpg/flat.mk
+++ src/pipe/modules/i-jpg/flat.mk
@@ -1,3 +1,9 @@
-MOD_CFLAGS=$(VKDT_JPEG_CFLAGS)
-MOD_LDFLAGS=$(VKDT_JPEG_LDFLAGS)
-pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h $(VKDT_JPEG_STATIC_DEPS)
+ifeq ($(VKDT_JPEG_STATIC), 1)
+  MOD_CFLAGS=-I$(LIBJPEG_TURBO_I) -I$(LIBJPEG_TURBO_L)
+  MOD_LDFLAGS=-L$(LIBJPEG_TURBO_L) -ljpeg
+  MOD_DEPS=$(LIBJPEG_TURBO_L)/libjpeg.a
+else
+  MOD_CFLAGS=$(VKDT_JPEG_CFLAGS)
+  MOD_LDFLAGS=$(VKDT_JPEG_LDFLAGS)
+endif
+pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h
