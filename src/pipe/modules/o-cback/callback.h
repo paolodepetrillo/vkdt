da465af30ce8d1bd6406cf80b423a17801723720
Paolo DePetrillo <paolod@gmail.com>
2023-12-11 19:25:58 -0500

    Add parameter

--- src/pipe/modules/o-cback/callback.h
+++ src/pipe/modules/o-cback/callback.h
@@ -4,6 +4,9 @@
 
 #include <stdint.h>
 
-typedef void (*dt_output_callback_t)(dt_token_t inst, int wd, int ht, const uint8_t *data);
+typedef void (*dt_output_callback_t)(void *param, dt_token_t inst, int wd, int ht, const uint8_t *data);
 
-extern dt_output_callback_t dt_output_callback;
+typedef struct {
+    dt_output_callback_t callback;
+    void *param;
+} dt_ocback_data_t;
