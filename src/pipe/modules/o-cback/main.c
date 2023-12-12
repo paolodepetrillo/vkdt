f9475ebdb761f57f31475dfe9da20233786dd969
Paolo DePetrillo <paolod@gmail.com>
2023-12-11 19:25:58 -0500

    Add parameter

--- src/pipe/modules/o-cback/main.c
+++ src/pipe/modules/o-cback/main.c
@@ -5,7 +5,8 @@ void write_sink(
     dt_module_t *module,
     void *buf)
 {
-  dt_output_callback_t cb = (dt_output_callback_t)module->data;
-  if(cb != NULL)
-    cb(module->inst, module->connector[0].roi.wd, module->connector[0].roi.ht, buf);
+  dt_ocback_data_t *data = (dt_ocback_data_t *)module->data;
+  if(data != NULL)
+    if (data->callback != NULL)
+      data->callback(data->param, module->inst, module->connector[0].roi.wd, module->connector[0].roi.ht, buf);
 }
