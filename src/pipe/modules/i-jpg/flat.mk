708e5fda177b73478a1f112261cf283e22ce9200
Paolo DePetrillo <paolod@gmail.com>
2023-12-16 23:55:53 -0500

    Remove static libjpeg

--- src/pipe/modules/i-jpg/flat.mk
+++ src/pipe/modules/i-jpg/flat.mk
@@ -1,9 +1,3 @@
-ifeq ($(VKDT_JPEG_STATIC), 1)
-  MOD_CFLAGS=-I$(LIBJPEG_TURBO_I) -I$(LIBJPEG_TURBO_L)
-  MOD_LDFLAGS=-L$(LIBJPEG_TURBO_L) -ljpeg
-  MOD_DEPS=$(LIBJPEG_TURBO_L)/libjpeg.a
-else
-  MOD_CFLAGS=$(VKDT_JPEG_CFLAGS)
-  MOD_LDFLAGS=$(VKDT_JPEG_LDFLAGS)
-endif
+MOD_CFLAGS=$(VKDT_JPEG_CFLAGS)
+MOD_LDFLAGS=$(VKDT_JPEG_LDFLAGS)
 pipe/modules/i-jpg/libi-jpg.so:pipe/modules/i-jpg/jpegexiforient.h
